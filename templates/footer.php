fb827cb6a72b4de64e5c3e6248361552b3609dea
Tony-Elia <antony.safaga@gmail.com>
2023-05-23 00:03:48 +0300

    Files Filtering and renaming

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 
         <script src="layout/js/bootstrap.min.js"></script>
 <!--        <script src="layout/js/all.min.js"></script>-->
-        <script src="layout/js/loginRegist.js"></script>
+        <script src="layout/js/Script.js"></script>
     </body>
 </html>
